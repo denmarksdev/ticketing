315ddef09015464d040f4b6406337d28f9c781b0
Denis <marksdev@gmail.com>
2023-05-17 07:48:03 -0300

    test: log

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,8 @@ import { app } from "./app";
 const Port = 3000;
 
 const start = async () => {
+  console.log('Starting up ...')
+
   if (! process.env.JWT_KEY){
     throw Error('JWT_KEY must be defined')
   } 
