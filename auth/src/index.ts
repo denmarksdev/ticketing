e1081682c0260f2c9238a6952de73cddebd7d344
Denis Marques <denmarksdev@gmail.com>
2023-05-19 07:37:37 -0300

    Dev (#6)
    * no message
    
    * test: trigget auth action
    
    * chore: trigget auth depl
    
    ---------
    
    Co-authored-by: Denis <marksdev@gmail.com>

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,7 +4,7 @@ import { app } from "./app";
 const Port = 3000;
 
 const start = async () => {
-  console.log('Starting up ...')
+  console.log('Starting up .....')
 
   if (! process.env.JWT_KEY){
     throw Error('JWT_KEY must be defined')
