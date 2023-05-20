19e5b5df79a19dbebd42014f188036ef963ac947
Denis <marksdev@gmail.com>
2023-05-19 22:35:47 -0300

    no message

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -7,6 +7,8 @@ import { OrderCreatedListener } from "./events/listeners/order-created-listener"
 const Port = 3000;
 
 const start = async () => {
+  console.log('Starting ....')
+
   if (!process.env.JWT_KEY) {
     throw Error("JWT_KEY must be defined");
   }
